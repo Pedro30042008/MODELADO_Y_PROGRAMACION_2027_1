5fa6da8d5326282efa79c270e079131379d6a7ba
323Alang <1alang2@ciencias.unam.mx>
2026-09-01 13:36:06 -0600

    feat: Mejorando Tupla

--- practica-01/src/Tupla.java
+++ practica-01/src/Tupla.java
@@ -1,33 +1,59 @@
 
-//clase tupla para asignar una llave a un valor
+/**
+ * Tupla
+ * 
+ * Clase destinada al mapeo de una llave a un valor
+ */
 public class Tupla {
     //ATRIBUTOS
     String valor;
     int llave;
 
-    //Constructor
+    /**
+     * Constructor basico para la crear una Tupla
+     * @param valor
+     * @param llave
+     */
     public Tupla(String valor, int llave) {
         this.valor = valor;
         this.llave = llave;
     }
 
-    //Getters and setters
+    /**
+     * Regresa el valor.
+     * @return valor
+     */
     public String getValor() {
         return valor;
     }
 
+    /**
+     * Asiganamos un valor
+     * @param valor
+     */
     public void setValor(String valor) {
         this.valor = valor;
     }
 
+    /**
+     * Regresa la llave asociada al valor
+     * @return
+     */
     public int getLlave() {
         return llave;
     }
 
+    /**
+     * Asignamos llave
+     * @param llave
+     */
     public void setLlave(int llave) {
         this.llave = llave;
     }
 
+    /**
+     * Representacion visula del objeto tupla
+     */
     @Override
     public String toString (){
         return "("+llave+", "+valor+")";
