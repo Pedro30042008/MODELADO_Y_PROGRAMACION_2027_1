418a3eb9daeb1dd9c4ef2f5347bec7afece6f686
323Alang <1alang2@ciencias.unam.mx>
2026-09-01 13:58:11 -0600

    feat: Base de la clase main

---
+++ practica-01/src/practica01.java
@@ -0,0 +1,9 @@
+/**
+ * practica01
+ * Clase main, donde se preguntara al usuario que desea hacer
+ */
+public class practica01 {
+    public static void main(String[] cositasLindas) {
+        System.out.println("First prueba");
+    }
+}
