5d80bd88f56ce201343711c338c53c09c3f498f4
PedroPablo <pedro.pablo.ruiz.hdez@ciencias.unam.mx>
2026-09-01 13:57:00 -0600

    feat: Esqueleto de la TablaHash

---
+++ practica-01/src/HashTable.java
@@ -0,0 +1,43 @@
+import java.util.LinkedList;
+
+/**
+ * HashTable.
+ * 
+ * Clase destinada a generar una HashTable.
+ *
+ */
+public class HashTable {
+
+    /* Tamaño de la TablaHash. */
+    private int dimension;
+
+    /* Arreglo de listas de tuplas. */
+    private LinkedList<Tupla>[] hashTable;
+
+    /**
+     * Método para insertar un valor dada una llave.
+     * @param int llave.
+     * @param String valor.
+     */
+    public void insertar(int llave, String valor) {
+	
+    }
+    
+    /**
+     * Método para buscar un valor dada una llave.
+     * @param int llave.
+     * @return String valor.
+     */
+    public String buscar(int llave) {
+	
+    }
+
+    /**
+     * Método para eliminar una tupla dada una llave.
+     * @param int llave.
+     */
+    public void eliminar(int llave) {
+	
+    }
+    
+}
