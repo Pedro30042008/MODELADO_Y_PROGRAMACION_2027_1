2e277f5045f96cfa40a7de9de61d98affa3bcc36
323Alang <1alang2@ciencias.unam.mx>
2026-09-01 13:26:31 -0600

    Incorporando clase tupla

--- practica-01/src/Tupla.java
+++ practica-01/src/Tupla.java
@@ -1,4 +1,37 @@
 
+//clase tupla para asignar una llave a un valor
 public class Tupla {
-    
+    //ATRIBUTOS
+    String valor;
+    int llave;
+
+    //Constructor
+    public Tupla(String valor, int llave) {
+        this.valor = valor;
+        this.llave = llave;
+    }
+
+    //Getters and setters
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    public int getLlave() {
+        return llave;
+    }
+
+    public void setLlave(int llave) {
+        this.llave = llave;
+    }
+
+    @Override
+    public String toString (){
+        return "("+llave+", "+valor+")";
+    }
+
+
 }
